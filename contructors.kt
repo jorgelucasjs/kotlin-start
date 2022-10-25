0455b15660577cc489e31d7be22e50826f111c71
Jorge LucasJs <jorge.tomasgt@gmail.com>
2022-10-25 13:38:56 +0100

    first commit

---
+++ contructors.kt
@@ -0,0 +1,47 @@
+
+// contrutor primario
+class Casa /*(cor: String, vagasGaragem:Int )*/{
+    
+    //propriedades
+    var cor: String
+    var vagasGaragem: Int
+    
+
+    /* init{
+        this.cor = cor
+        this.vagasGaragem = vagasGaragem
+    }*/
+    
+	// contrutor secundario
+    constructor(cor: String, vagasGaragem:Int){
+        this.cor = cor
+        this.vagasGaragem = vagasGaragem
+    }
+    
+    //Metodos
+    fun detalhesCasa(){
+        println("A casa tem cor: $cor, vagas: $vagasGaragem")
+    }
+    
+    fun abrirJanela (){
+        println("Abrir Janela")
+    }
+    
+    fun abrirPorta (){
+        println("Abrir Porta")
+    }
+    
+    fun abrirCasa(){
+        this.abrirJanela()
+        this.abrirPorta()
+    }
+
+}
+
+fun main() {
+	
+    val casa = Casa("Amarela", 2)
+    casa.detalhesCasa()
+
+
+}
