ec18370cbe0af46b646731def5a5c9d7cfee19b7
Jorge LucasJs <jorge.tomasgt@gmail.com>
2022-11-22 10:17:26 +0100

    first android app

---
+++ Android com Kotlin/Criando seu primeiro App/Orgs/app/src/main/java/br/com/alura/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -0,0 +1,43 @@
+package br.com.alura.orgs.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import br.com.alura.orgs.R
+import br.com.alura.orgs.model.Produto
+import java.math.BigDecimal
+
+class FormularioProdutoActivity :
+    AppCompatActivity(R.layout.activity_formulario_produto) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val botaoSalvar = findViewById<Button>(R.id.botao_salvar)
+        botaoSalvar.setOnClickListener {
+
+            val campoNome = findViewById<EditText>(R.id.nome)
+            val nome = campoNome.text.toString()
+
+            val campoDescricao = findViewById<EditText>(R.id.descricao)
+            val descricao = campoDescricao.text.toString()
+
+            val campoValor = findViewById<EditText>(R.id.valor)
+            val valorEmTexto = campoValor.text.toString()
+
+            val valor = if(valorEmTexto.isBlank()){
+                BigDecimal.ZERO
+            }else{
+                BigDecimal(valorEmTexto)
+            }
+
+            val produtoNovo = Produto(nome, descricao, valor)
+
+            Log.i("FormularioProduto", "Oncreate $produtoNovo")
+        }
+    }
+
+}
